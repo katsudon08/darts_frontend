2b798955fd19155e3aa983d52d5bf3c0e116c5b8
matsu <matsuaki171101@gmail.com>
2023-12-05 20:20:35 +0900

    eventの伝播を解消する必要がある

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
 'use client'
 
+import React from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
 	const router = useRouter()
 
-	const handleClick = () => {
-		router.push('/room-setting')
+	const handleClick = (e: React.TouchEvent) => {
+		// stopPropagationが動かない
+		e.stopPropagation()
+		// router.push('/room-setting')
+		console.log("route")
 	}
 
 	return (
 		<main>
-			<div className=" flex pt-10 bg-lime-600 min-h-screen justify-center items-center " onTouchEnd={handleClick}>
+			<div className=" flex pt-10 bg-lime-600 min-h-screen justify-center items-center " onTouchEnd={(e: React.TouchEvent) => handleClick(e)}>
 				<div className=" flex flex-col pt-10 min-h-fit p-2 h-60 justify-between ">
-					<input type="text"className=" py-4 px-2 "/>
+					<input type="text"className=" py-4 px-2 text-xl "/>
 					<h1 className=" whitespace-nowrap text-4xl font-semibold ">タップしてスタート</h1>
 				</div>
 			</div>
