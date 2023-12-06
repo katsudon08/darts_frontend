33a894677dfbe4f1a1293bad6d8998f966e86d27
matsu <matsuaki171101@gmail.com>
2023-12-06 13:53:42 +0900

    イベント発火の伝播を解消

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,20 @@
 'use client'
 
-import React from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
 	const router = useRouter()
 
-	const handleClick = (e: React.TouchEvent) => {
-		// stopPropagationが動かない
-		e.stopPropagation()
-		// router.push('/room-setting')
-		console.log("route")
+	const handleOuterTap = () => {
+		router.push('/room-setting')
 	}
 
 	return (
 		<main>
-			<div className=" flex pt-10 bg-lime-600 min-h-screen justify-center items-center " onTouchEnd={(e: React.TouchEvent) => handleClick(e)}>
+			<div className=" flex pt-10 bg-lime-600 min-h-screen justify-center items-center " onClick={handleOuterTap}>
 				<div className=" flex flex-col pt-10 min-h-fit p-2 h-60 justify-between ">
-					<input type="text"className=" py-4 px-2 text-xl "/>
-					<h1 className=" whitespace-nowrap text-4xl font-semibold ">タップしてスタート</h1>
+					<input type="text"className=" py-4 px-2 text-xl " onClick={e => e.stopPropagation()}/>
+					<h1 className=" whitespace-nowrap select-none text-4xl font-semibold ">タップしてスタート</h1>
 				</div>
 			</div>
 		</main>
