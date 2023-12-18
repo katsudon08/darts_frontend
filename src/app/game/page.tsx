804f84ccba76d633520bd6b94e93e5ba2c08aaac
matsu <matsuaki171101@gmail.com>
2023-12-18 21:21:22 +0900

    ダーツ板のように文字を配置することができたが、回転してしまっているため修正する必要あり

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -4,7 +4,17 @@ import TEXT from "@/components/TEXT"
 import { useState } from "react"
 
 export default function () {
-    const [isActive, setIsActive] = useState(false)
+    const [isActive, setIsActive] = useState(true)
+
+    const darts = [
+        [12, 5, 20, 1, 18],
+        [9, 0, 0, 0, 4],
+        [14, 0, 0, 0, 13],
+        [11, 0, 0, 0, 6],
+        [8, 0, 0, 0, 10],
+        [16, 0, 0, 0, 15],
+        [7, 19, 3, 17, 2]
+    ]
 
     const handleDarts = (e: React.MouseEvent) => {
         setIsActive(!isActive)
@@ -36,14 +46,112 @@ export default function () {
                                 : "flex flex-col justify-center items-center h-80 w-80 invisible"
                         }
                     >
-                        {Array(7).fill(0).map((v, i) => (
-                            <div className="bg-blue-600 flex flex-row visible h-full w-full border border-black" key={i}>
-                                {Array(7).fill(0).map((v, i) => (
-                                    <div className="h-full w-full border border-b-zinc-600" key={i}></div>
-                                ))}
+
+                        <div className="relative visible translate-y-1/4 bg-blue-950 h-5 w-5">
+                            <div className="absolute top-1 visible h-40 w-8 -translate-y-full -translate-x-3">
+                                <button className="flex justify-center h-full w-fit pt-1">
+                                    <TEXT text="20" />
+                                </button>
                             </div>
-                        ))}
-                        <div className="absolute visible h-32 w-32 rounded-full bg-red-400">
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-18">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="1" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-36">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="18" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-54">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="4" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-72">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="13" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-90">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="6" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-108">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="10" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-126">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="15" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-144">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="2" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-162">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="17" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-180">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="3" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-198">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="19" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-216">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="7" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-234">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="16" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-252">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="8" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-270">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="11" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-288">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="14" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-306">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="9" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-324">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="12" />
+                                </button>
+                            </div>
+                            <div className="absolute text-center top-1 visible h-40 w-12 -translate-y-full -translate-x-3 origin-bottom rotate-342">
+                                <button className="flex justify-center h-full w-full pt-1">
+                                    <TEXT text="5" />
+                                </button>
+                            </div>
+                        </div>
+
+
+                        <div className="absolute invisible h-32 w-32 rounded-full bg-red-400">
                             <button className="h-full w-full" onClick={e => handleDarts(e)} />
                         </div>
                     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,27 @@ const config: Config = {
                 "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
                 "gradient-conic":
                     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+            },
+            rotate: {
+                "18": "18deg",
+                "36": "36deg",
+                "54": "54deg",
+                "72": "72deg",
+                "90": "90deg",
+                "108": "108deg",
+                "126": "126deg",
+                "144": "144deg",
+                "162": "162deg",
+                "198": "198deg",
+                "216": "216deg",
+                "234": "234deg",
+                "252": "252deg",
+                "270": "270deg",
+                "288": "288deg",
+                "306": "306deg",
+                "324": "324deg",
+                "342": "342deg",
+                "360": "360deg"
             }
         }
     },
